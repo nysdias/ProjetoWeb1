cf587ae134549c1fa99523b987dc5d6fcad08904
Nicole <nicoleesdias@gmail.com>
2026-05-14 19:54:54 -0300

    feat: Criando a model login

---
+++ ProjetoWeb1/Models/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjetoWeb1.Models
+{
+    public class LoginViewModel
+    {
+        // encapsulamento - modificadores get; set;
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+        public string Nivel { get; set; } = "Funcionario";
+    }
+}
