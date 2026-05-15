d622ea905176ce6d272283e21c9b99bd3201c3b1
Nicole <nicoleesdias@gmail.com>
2026-05-14 21:15:48 -0300

    feat: UsuarioRepositorio

---
+++ ProjetoWeb1/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using ProjetoWeb1.Models;
+
+namespace ProjetoWeb1.Repositorios
+{
+    public class UsuarioRepositorio(IConfiguration config)
+    {
+        private readonly string _connectionString = config.GetConnectionString("Conexão");
+
+        public LoginViewModel Validar(string email, string senha) 
+        {
+            using var conn = new MySqlConnection(_connectionString);
+        
+        }
+    }
+}
